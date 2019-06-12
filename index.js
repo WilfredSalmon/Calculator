bcc3df05ec524d4e8332beaecf71dcbb0ac3b0a8
WilfredSalmon <was29@cam.ac.uk>
2019-06-12 16:06:39 +0100

    Added multiplication

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Calculator init')
+const readline = require('readline-sync');
+
+console.log('Calculator init \nPlease enter two numbers to multiply');
+console.log('Number 1: ');
+var x = readline.prompt();
+x = parseFloat(x);
+
+console.log('Number 2: ');
+var y = readline.prompt();
+y= parseFloat(y);
+
+console.log(x*y)
