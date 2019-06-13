9404124d54d67058728393977053acfb1e5c2c19
WilfredSalmon <was29@cam.ac.uk>
2019-06-13 12:34:44 +0100

    Operator choice added with switch statement

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const readline = require('readline-sync');
 
-console.log('Calculator init \nPlease enter two numbers to multiply');
+console.log('Calculator init \nPlease enter an operator and 2 numbers');
+console.log('Operator')
+const op = readline.prompt(); 
+
 console.log('Number 1: ');
 var x = readline.prompt();
 x = parseFloat(x);
@@ -9,4 +12,20 @@ console.log('Number 2: ');
 var y = readline.prompt();
 y= parseFloat(y);
 
-console.log(x*y)
+switch (op) {
+    case '+':
+        console.log(x+y);
+        break;
+    case '/':
+        console.log(x/y);
+        break;
+    case '*':
+        console.log(x*y);
+        break;
+    case '-':
+        console.log(x-y);
+        break;
+    default:
+        console.log('Sorry, \"'+ op +'\" is not a supported operator, please enter one of +,-,* or /');
+        break;
+}
