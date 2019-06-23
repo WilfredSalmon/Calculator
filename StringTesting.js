e06165be18d9eaa9a682b3f607d722adc023881f
WilfredSalmon <was29@cam.ac.uk>
2019-06-23 23:08:20 +0100

    added vowel counting

---
+++ StringTesting.js
@@ -0,0 +1,4 @@
+var string = 'a'
+string = string.slice(0,string.lastIndexOf('a'));
+console.log(string);
+console.log(string.length);
--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const readline = require('readline-sync');
 
 // FUNCTIONS //
 
+// Get an input
+function getInput(message) {
+    console.log(message);
+    return readline.prompt();
+}
+
 // Check if a variable is a number
 function IsNo(input) {
     
     const test = +input;
 
-    if (isNaN(input)) {
+    if (isNaN(test)) {
         return false;
     } else {
         return true;
@@ -18,8 +24,7 @@ function IsNo(input) {
 // Get a user inputed number prompted by message
 function getNumber(message) {
     
-    console.log(message);
-    const input = readline.prompt();
+    const input = getInput(message);
 
     if (IsNo(input)) {
         const number = +input;
@@ -38,9 +43,8 @@ function printWelcome() {
 
 // Gets the operator
 function getOperator() {
-    console.log('Enter an operator please')
-    const op = readline.prompt();
-    const supportedOps = ['+','-','*','/'] ;
+    const supportedOpsString = supportedOps.join(', ');
+    const op = getInput(`Enter an operator please. Supported operators are ${supportedOpsString}.`);
     
     if (supportedOps.includes(op)) {
         return op;
@@ -82,18 +86,18 @@ function getOperatorFunction(op) {
 
 // Gets the list of numbers to perform the operator to
 function getNos(op) {
-    const toEnter = getNumber('How many numbers do you want to '+op);
+    const toEnter = getNumber(`How many numbers do you want to ${op}?`);
     var nos = Array(toEnter);
 
     for (var i = 1; i<=toEnter; i++) {
-        nos[i-1]= getNumber('Number ' + i);
+        nos[i-1]= getNumber(`Number ${i}:`);
     }
 
     return nos;
 }
 
 // Gets the answer to the calculation
-function getAns(op,nos) {
+function getNoAns(op,nos) {
     const opFunction = getOperatorFunction(op);
 
     let ans = nos[0];
@@ -106,18 +110,73 @@ function getAns(op,nos) {
 }
 
 //Does a calculation
-function doCalc() {
+function doNoCalc() {
     const op = getOperator();
     const nos = getNos(op);
-    const ans = getAns(op,nos)
+    const ans = getNoAns(op,nos)
 
-    console.log('The answer is: ' + ans +'\n');
+    console.log(`the answer is: ${ans}\n`);
 }
+
+//Prints the properties of an object
+function printObject(obj) {
+    var result = `The character counts are:\n\n`;
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            result += `${i}: ${obj[i]}\n`;
+        }
+    }
+    console.log(result)
+}
+
+//Does a vowel calculation
+function doVowelCalc() {
+    const phrase = getInput('Enter a string to count the number of vowels in')
+    const phraseUpperCased = phrase.toUpperCase();
+    var ans = new Object();
+    
+    for (i=0;i<charsToCount.length;i++) {
+        const char = charsToCount[i];
+        var count = 0;
+        var string = phraseUpperCased;
+        
+        while (string.lastIndexOf(char) >= 0) {
+            string = string.slice(0,string.lastIndexOf(char));
+            count++
+        }
+
+        ans[char] = count;
+
+    }
+
+    printObject(ans);
+
+}
+
+/////////////////////////////////////////////////////////////
+// VARS
+const supportedOps = ['+','-','*','/'] ;
+const numberMode = 1;
+const wordMode = 2;
+const menu = `Which calculator mode do you want?
+1) Arithmetic
+2) Vowel counting`
+var charsToCount = ['A','E','I','O','U'];
+
+
 
 //MAIN CODE//
 printWelcome();
 
 //Start main loop
 while (true) {
-    doCalc();
+    const mode = getInput(menu);
+
+    if (mode === '1') {
+        doNoCalc();
+    } else if (mode == '2') {
+        doVowelCalc();       
+    } else {
+        console.log('Mode not recognised, please try again\n')
+    }
 }
